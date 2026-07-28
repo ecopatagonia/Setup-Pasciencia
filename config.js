e4d4e5d187eb51053fda6907de56dd6100cedf43
ecopatagonia <mbrucoli@gmail.com>
2026-07-28 12:25:32 -0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.PULO_ACCESS_CONFIG = Object.freeze({
-  API_URL: "https://script.google.com/macros/s/AKfycbzVn3KIKYAw-perSNWghnBtC49oghCeNBLoxsQDmY_Mopk2TJ4sxhuaEOWYtJ7pVWHRaA/exec",
-  ADMIN_URL: "admin/",
-  SITE_URL: "../?view=site",
-  REGISTRATION_URL: "registro/",
-  TOKEN_KEY: "pulo_gatto_session",
-  SESSION_CHECK_MS: 60000
-});
