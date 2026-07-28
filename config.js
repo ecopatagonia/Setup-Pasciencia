bf5cbc8a4fa1cfc153c361e69e8b1b23577a67b2
ecopatagonia <mbrucoli@gmail.com>
2026-07-28 12:25:57 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.PULO_ACCESS_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycbzVn3KIKYAw-perSNWghnBtC49oghCeNBLoxsQDmY_Mopk2TJ4sxhuaEOWYtJ7pVWHRaA/exec",
+  ADMIN_URL: "admin/index.html",
+  SITE_URL: "../index.html?view=site",
+  REGISTRATION_URL: "registro/index.html",
+  TOKEN_KEY: "pulo_gatto_session",
+  SESSION_CHECK_MS: 60000
+});
